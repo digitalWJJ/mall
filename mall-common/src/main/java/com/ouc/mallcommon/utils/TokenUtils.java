67ad7c460a463524809f537db3ff1cd2c9db57ad
王佳俊 <2568840553@qq.com>
2023-11-24 00:25:34 +0800

    feat: token工具的实现

---
+++ mall-common/src/main/java/com/ouc/mallcommon/utils/TokenUtils.java
@@ -0,0 +1,55 @@
+package com.ouc.mallcommon.utils;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.ouc.mallmbg.mapper.UserMapper;
+import com.ouc.mallmbg.model.User;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Date;
+
+public class TokenUtils {
+
+    @Autowired
+    private static UserMapper staticUserMapper;
+
+    @Resource
+    UserMapper userMapper;
+
+    @PostConstruct
+    public void setStaticUserMapper(){
+        staticUserMapper = userMapper;
+    }
+
+    /*
+     * 生成token
+     * */
+    public static String genToken(String userId, String sign){
+        return JWT.create().withAudience(userId)
+                .withExpiresAt(DateUtil.offsetHour(new Date(),2))
+                .sign(Algorithm.HMAC256(sign)); // 以 password 作为 token 的密钥
+    }
+
+    public static User getCurrentUser(){
+        try{
+            HttpServletRequest request = ( (ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String token = request.getHeader("Authorization");
+            if(StrUtil.isNotBlank(token)){
+                token = token.replace("Bearer","");
+                String userId = JWT.decode(token).getAudience().get(0);
+                return staticUserMapper.selectByPrimaryKey(Integer.getInteger(userId));
+            }
+        } catch (Exception e){
+            return null;
+        }
+        return null;
+    }
+
+}
