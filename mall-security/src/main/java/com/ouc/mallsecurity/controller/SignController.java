11000b61bf03d5a1ddc94000a8ec0af32e5fbf47
王佳俊 <2568840553@qq.com>
2023-11-24 00:26:36 +0800

    feat: 登陆注册控制器的初始化

--- mall-security/src/main/java/com/ouc/mallsecurity/controller/SignController.java
+++ mall-security/src/main/java/com/ouc/mallsecurity/controller/SignController.java
@@ -2,19 +2,31 @@ package com.ouc.mallsecurity.controller;
 
 import com.ouc.mallcommon.Result;
 import com.ouc.mallmbg.mapper.UserMapper;
+//import com.ouc.mallsecurity.model.CodeModel;
+//import com.ouc.mallsecurity.model.PwdModel;
 import com.ouc.mallsecurity.model.CodeModel;
 import com.ouc.mallsecurity.model.PwdModel;
+import com.ouc.mallsecurity.service.SignService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SignController {
+    @Autowired
+    private SignService signService;
 
     @RequestMapping("/test")
     public Result test(){
+        return Result.success();
+    }
+
+    /*
+    * 测试邮箱验证码功能
+    * */
+    @GetMapping("/getEmailCode")
+    public Result getEmailCode(@RequestParam("email") String email){
+        System.out.println(email);
+        signService.sendEmailCode(email);
         return Result.success();
     }
 
