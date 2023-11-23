d312012106425f5989ef5c15d844cec0bb489342
王佳俊 <2568840553@qq.com>
2023-11-24 00:23:56 +0800

    feat: 实现了发送邮件的服务

---
+++ mall-security/src/main/java/com/ouc/mallsecurity/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.ouc.mallsecurity.service;
+
+import com.ouc.mallsecurity.model.EmailModel;
+
+public interface EmailService {
+
+    /*
+    * 发送邮件
+    * @param emailModel 发送的邮箱对象
+    * */
+    void send(EmailModel emailModel);
+}
---
+++ mall-security/src/main/java/com/ouc/mallsecurity/service/impl/EmailServiceImpl.java
@@ -0,0 +1,60 @@
+package com.ouc.mallsecurity.service.impl;
+
+import cn.hutool.extra.mail.Mail;
+import cn.hutool.extra.mail.MailAccount;
+import com.ouc.mallcommon.exception.ServiceException;
+import com.ouc.mallsecurity.model.EmailModel;
+import com.ouc.mallsecurity.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Value("${spring.mail.email}")
+    private String email;
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private String port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Override
+    public void send(EmailModel emailModel) {
+
+
+        MailAccount account = new MailAccount();
+
+        account.setHost(host);
+        account.setPort(Integer.parseInt(port));
+        // 设置发送人邮箱
+        account.setFrom(username + "<" + email + ">" );
+        // 设置发送人名称
+        account.setUser(username);
+        // 设置发送授权码
+        account.setPass(password);
+        account.setAuth(true);
+        // ssl方式发送
+        account.setSslEnable(true);
+        // 使用安全连接
+        account.setStarttlsEnable(true);
+
+        try{
+//            int size = emailModel.getTos();
+            Mail.create(account)
+                    .setTos(emailModel.getTos())
+                    .setTitle(emailModel.getSubject())
+                    .setContent(emailModel.getContent())
+                    .setHtml(true)
+                    .setUseGlobalSession(false)
+                    .send();
+        } catch (Exception e){
+            throw new ServiceException(500,e.getMessage());
+        }
+    }
+}
