7b32cab3c2e2a3197b197c0b7577ffaf812d4004
王佳俊 <2568840553@qq.com>
2023-11-26 16:44:09 +0800

    style: 添加注释

--- mall-security/src/main/java/com/ouc/mallsecurity/service/SignService.java
+++ mall-security/src/main/java/com/ouc/mallsecurity/service/SignService.java
@@ -1,30 +1,39 @@
 package com.ouc.mallsecurity.service;
 
+import com.ouc.mallmbg.model.User;
 import com.ouc.mallsecurity.model.CodeModel;
 import com.ouc.mallsecurity.model.PwdModel;
 import com.ouc.mallsecurity.model.SignModel;
 
 public interface SignService {
 
-    /*
+    /**
     * 向指定邮箱发送验证码
+    *
+    * @param email 接收验证码的邮箱
     * */
     void sendEmailCode(String email);
 
-    /*
-    * 通过账号密码来登录
-    * @return token
-    * */
+    /**
+     * 通过账号密码来登录
+     *
+     * @param pwdModel 密码登陆对象
+     * @return token
+     * */
     String signInByPwd(PwdModel pwdModel);
 
-    /*
-    * 通过邮箱验证码来登录
-    * @return token
-    * */
+    /**
+     * 通过邮箱验证码来登录
+     *
+     * @param codeModel 验证码登陆对象
+     * @return token
+     * */
     String signInByCode(CodeModel codeModel);
 
-    /*
-    * 注册
-    * */
+    /**
+     * 注册
+     *
+     * @param signModel 注册对象
+     * */
     void signUp(SignModel signModel);
 }
--- mall-security/src/main/java/com/ouc/mallsecurity/service/UserService.java
+++ mall-security/src/main/java/com/ouc/mallsecurity/service/UserService.java
@@ -3,18 +3,31 @@ package com.ouc.mallsecurity.service;
 import com.ouc.mallmbg.model.User;
 
 public interface UserService {
-    /*
-    * 通过邮箱检索用户
-    * */
+    /**
+     * 通过邮箱检索用户
+     *
+     * @return 有用户就返回 user 没有就抛出 ServiceException
+     * */
     User findByUserEmail(String email);
 
-    /*
-    * 添加一个新用户
-    * */
+    /**
+     * 添加一个新用户
+     *
+     * @param user 需要被添加的用户对象
+     * */
     void addUser(User user);
 
-    /*
-    * 验证用户密码是否正确
-    * */
+    /**
+     * 验证用户密码是否正确
+     *
+     * @return true or false
+     * */
     boolean pwdValidate(String userEmail, String userPwd);
+
+    /**
+     * 验证是否存在指定邮箱的用户
+     *
+     * @param email 邮箱
+     * */
+    boolean isUserExist(String email);
 }
