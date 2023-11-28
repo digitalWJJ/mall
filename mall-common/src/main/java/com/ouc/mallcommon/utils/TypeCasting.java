2b084be0747d872f746734fa5e4acca573d9438e
152******51 <10219428+dlh11234@user.noreply.gitee.com>
2023-11-28 08:31:36 +0800

    补充

--- mall-common/src/main/java/com/ouc/mallcommon/utils/TypeCasting.java
+++ mall-common/src/main/java/com/ouc/mallcommon/utils/TypeCasting.java
@@ -21,15 +21,24 @@ public class TypeCasting {
         }
         return date;
     }
+    public static List<String> imgNameToImgURL(List<String> imgNameList) throws ClientException {
+        List<String> imgUrlLit=new ArrayList<>();
+        for(String imgName:imgNameList)
+        {
+            imgUrlLit.add(OSS.getProductImgUrl(imgName,0));
+        }
+        return imgUrlLit;
+    }
     public static SplitProduct productToSplitProduct(Product product) throws ClientException {
         List<String> colors= List.of(product.getColor().toString().split("|"));
         List<String> configurations=List.of(product.getConfiguration().split("|"));
         List<String> productImages=new ArrayList<>();
-        if(product.getProductImage1()!=null)productImages.add(OSS.getProductImgUrl(product.getProductImage1(),0));
-        if(product.getProductImage2()!=null)productImages.add(OSS.getProductImgUrl(product.getProductImage2(),0));
-        if(product.getProductImage3()!=null)productImages.add(OSS.getProductImgUrl(product.getProductImage3(),0));
-        if(product.getProductImage4()!=null)productImages.add(OSS.getProductImgUrl(product.getProductImage4(),0));
-        if(product.getProductImage5()!=null)productImages.add(OSS.getProductImgUrl(product.getProductImage5(),0));
+        if(product.getProductImage1()!=null)productImages.add(product.getProductImage1());
+        if(product.getProductImage2()!=null)productImages.add(product.getProductImage2());
+        if(product.getProductImage3()!=null)productImages.add(product.getProductImage3());
+        if(product.getProductImage4()!=null)productImages.add(product.getProductImage4());
+        if(product.getProductImage5()!=null)productImages.add(product.getProductImage5());
+        productImages=imgNameToImgURL(productImages);
         return new SplitProduct(product.getId(),product.getProductName(),product.getProductDescription(),product.getProductPrice(),configurations,colors,productImages,product.getCategory());
     }
 }
