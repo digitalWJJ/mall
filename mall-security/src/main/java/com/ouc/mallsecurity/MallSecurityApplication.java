aabd8476060eba883f6b3bf3f458117a3b7635e6
王佳俊 <2568840553@qq.com>
2023-11-30 18:28:19 +0800

    fix: 整合 OSS 工具类

--- mall-security/src/main/java/com/ouc/mallsecurity/MallSecurityApplication.java
+++ mall-security/src/main/java/com/ouc/mallsecurity/MallSecurityApplication.java
@@ -1,9 +1,11 @@
 package com.ouc.mallsecurity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ouc")
+@MapperScan(value = "com.ouc.mallmbg.mapper")
 public class MallSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallSecurityApplication.class, args);
