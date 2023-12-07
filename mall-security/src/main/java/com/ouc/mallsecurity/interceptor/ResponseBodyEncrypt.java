628e6740bae1466d02c3d7cc688296ddb0e9c36f
王佳俊 <2568840553@qq.com>
2023-12-07 21:35:22 +0800

    fix: 修复了无法正常处理请求头中不携带公钥的情况

--- mall-security/src/main/java/com/ouc/mallsecurity/interceptor/ResponseBodyEncrypt.java
+++ mall-security/src/main/java/com/ouc/mallsecurity/interceptor/ResponseBodyEncrypt.java
@@ -1,8 +1,8 @@
 package com.ouc.mallsecurity.interceptor;
 
+import cn.hutool.core.net.URLDecoder;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import com.ouc.mallcommon.Result;
 import com.ouc.mallsecurity.model.EnDeModel;
 import com.ouc.mallsecurity.utils.AesUtil;
 import com.ouc.mallsecurity.utils.RsaUtil;
@@ -22,7 +22,7 @@ import java.nio.charset.StandardCharsets;
  * string   ->   string   ->   byte   ->   byte   ->   byte   ->   string
  * 明文string   aes加密之后的   转为byte    rsa加密的     base64转码的   传输的
  * */
-//@ControllerAdvice
+@ControllerAdvice
 public class ResponseBodyEncrypt implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
@@ -35,6 +35,8 @@ public class ResponseBodyEncrypt implements ResponseBodyAdvice<Object> {
     @Override
     public JSONObject beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
 
+        response.getHeaders().set("Access-Control-Expose-Headers", "ServerPublicKey"); // 解决axios 无法受到自定义的请求头
+        response.getHeaders().set("ServerPublicKey", RsaUtil.getPublicKey()); // 在响应头中设置服务器端的公钥
         String jsonString = JSONUtil.toJsonStr(body);
         String aesKey = AesUtil.getKey(); // aesKey 每一次 get 都是新的 所以需要先缓存
         System.out.println(aesKey); // TODO 打印输出
@@ -42,8 +44,9 @@ public class ResponseBodyEncrypt implements ResponseBodyAdvice<Object> {
         try {
             String load = AesUtil.encrypt(jsonString, aesKey);
             enDeModel.setLoad(load); // 设置载荷部分
-            // TODO 公钥需要更换为前端给我的公钥
-            byte[] enAesKey = RsaUtil.encryptByPublicKey(aesKey.getBytes(StandardCharsets.UTF_8), RsaUtil.getPublicKey()); // 获取加密后的 byte
+            String ClientPublicKey = URLDecoder.decode( request.getHeaders().getFirst("ClientPublicKey"), StandardCharsets.UTF_8);
+            if(ClientPublicKey == null) return null; // 没有客户端公钥就返回null
+            byte[] enAesKey = RsaUtil.encryptByPublicKey(aesKey.getBytes(StandardCharsets.UTF_8), ClientPublicKey); // 获取加密后的 byte
             enDeModel.setEncryptedAesKey(Base64.encodeBase64String(enAesKey));
         } catch (Exception e) {
             throw new RuntimeException(e);
